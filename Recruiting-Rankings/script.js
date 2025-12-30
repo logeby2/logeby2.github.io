184e7bd277f768f7064c70cb8200710ccc227fbc
logeby2 <logeby2@gmail.com>
2025-12-30 14:30:42 -0600

    international insanity

--- Recruiting-Rankings/script.js
+++ Recruiting-Rankings/script.js
@@ -84,7 +84,8 @@ document.addEventListener('DOMContentLoaded', () => {
                         'TUR': 'tr', 'GRE': 'gr', 'NGA': 'ng', 'CRO': 'hr', 'DEN': 'dk',
                         'URU': 'uy', 'PUR': 'pr', 'ARG': 'ar', 'CHN': 'cn', 'GIN': 'gn', 'GUI': 'gn', 'BUR': 'bf',
                         'CZA': 'cz', 'CZE': 'cz', 'BIH': 'ba', 'POR': 'pt', 'AUT': 'at',
-                        'JPN': 'jp', 'DOM': 'do', 'POL': 'pl', 'COD': 'cd', 'BFA': 'bf', 'GHA': 'gh', 'ISR': 'il', 'NZL': 'nz'
+                        'JPN': 'jp', 'DOM': 'do', 'POL': 'pl', 'COD': 'cd', 'BFA': 'bf', 'GHA': 'gh', 'ISR': 'il', 'NZL': 'nz',
+                        'IND': 'in', 'VEN': 've', 'UKR': 'ua'
                     };
                     flag = `https://flagcdn.com/20x15/${countryMap[code] || 'us'}.png`;
                 }
